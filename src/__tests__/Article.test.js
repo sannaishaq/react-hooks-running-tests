9d35737b32e8735e605f106832932e6fc8207fd0
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-03-12 23:10:16 -0500

    Done!

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,12 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+ 
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
